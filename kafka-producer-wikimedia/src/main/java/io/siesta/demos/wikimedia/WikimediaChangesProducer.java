fe34eece6fc60a8f0ec2b2ae8475927be5a58f71
Ashish Raj <rajs22007@gmail.com>
2023-12-05 03:26:52 +0530

    safe producer and high throughput producer configs

--- kafka-producer-wikimedia/src/main/java/io/siesta/demos/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/siesta/demos/wikimedia/WikimediaChangesProducer.java
@@ -20,10 +20,21 @@ public class WikimediaChangesProducer {
     public static void main(String[] args) {
         LOGGER.info("Hello World!");
 
+        // create producer config
         Properties properties = new Properties();
         properties.setProperty(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // set safe producer configs (kafka <= 2.8)
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all"); // same as setting -1
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+
+        // set high throughput producer configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
 
         String topic = "wikimedia.recentchange";
         String url = "https://stream.wikimedia.org/v2/stream/recentchange";
